2bef08349d1146617105050007b0572e8bf3a476
Alexandr Bulanov <bulya911@gmail.com>
2013-09-28 15:02:24 +0300

    imports fixed

--- backend/allmywishes/apps/accounts/models.py
+++ backend/allmywishes/apps/accounts/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.template.defaultfilters import slugify
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django_extensions.db.fields import AutoSlugField
-
-from unidecode import unidecode
 
 
 class AccountManager(BaseUserManager):
