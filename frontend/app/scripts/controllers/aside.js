dc7da316f1927c0076993c315b3b54ee6f15f56f
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-09-28 13:29:54 +0300

    Added skeleton for account service and aside controller.

---
+++ frontend/app/scripts/controllers/aside.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('frontendApp')
+  .controller('AsideCtrl', function ($scope) {
+    $scope.testContent = 'Text from ctrl';
+  });
---
+++ frontend/app/scripts/services/account.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('frontendApp')
+  .service('Account', function Account() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
