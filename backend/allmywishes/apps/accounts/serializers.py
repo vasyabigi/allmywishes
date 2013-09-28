c8271a4161340ce40dc7946bb1494dd3d5ac83e7
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-09-28 16:29:17 +0300

    Added account image.

--- backend/allmywishes/apps/accounts/serializers.py
+++ backend/allmywishes/apps/accounts/serializers.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 class AccountInfoSerializer(serializers.ModelSerializer):
     facebook_id = serializers.Field(source="facebook_id")
     is_authenticated = serializers.Field(source="is_authenticated")
+    image = serializers.Field(source="get_avatar_url")
 
     class Meta:
         model = Account
