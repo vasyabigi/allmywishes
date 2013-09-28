7c96027781a48a7f71aa259a062a95d340ff47de
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-09-28 10:09:05 +0300

    Initial django skeleton.

---
+++ backend/allmywishes/settings/common.py
@@ -0,0 +1,139 @@
+from os.path import abspath, dirname, join
+import sys
+
+PROJECT_PATH = dirname(dirname(abspath(__file__)))
+sys.path.append(join(PROJECT_PATH, 'apps'))
+
+PUBLIC_PATH = join(PROJECT_PATH, 'public')
+
+DEBUG = False
+TEMPLATE_DEBUG = True
+
+ADMINS = (
+    # ('', ''),
+)
+
+MANAGERS = ADMINS
+
+TIME_ZONE = 'UTC'
+
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+MEDIA_ROOT = join(PUBLIC_PATH, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = join(PUBLIC_PATH, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    join(PROJECT_PATH, 'static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'allmywishes.urls'
+
+WSGI_APPLICATION = 'allmywishes.wsgi.application'
+
+TEMPLATE_DIRS = (
+    join(PROJECT_PATH, 'templates'),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+)
+
+FIXTURE_DIRS = (
+    join(PROJECT_PATH, 'fixtures'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    # Plugins:
+    'south',
+
+    # Apps:
+    'core',
+)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': join(PROJECT_PATH, 'logs', 'vasyabigi.log'),
+            'maxBytes': 1024 * 1024 * 10,
+            'backupCount': 50,
+            'formatter': 'standard',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
