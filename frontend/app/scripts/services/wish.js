0dcd76580c6bc1b591040e5ac0e5f085cb9c6217
Alexandr Bulanov <bulya911@gmail.com>
2013-09-29 15:24:21 +0300

    service added

---
+++ frontend/app/scripts/services/wish.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('frontendApp')
+  .factory('$wish', ['$http', '$route', '$rootScope', '$q',
+    function ($http, $route, $rootScope, $q) {
+
+      var parseApiUrl = '/api/wishes/parse';
+
+      return {
+        parse: function(url) {
+          var parseDeffered = $q.defer();
+
+          $http.get(parseApiUrl, {params: {url: url}}).success(function(response) {
+            parseDeffered.resolve(response);
+          });
+
+          return parseDeffered.promise;
+        }
+      }
+    }]);
