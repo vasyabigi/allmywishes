9707844fec507a97d9a939ea4ebd05e210f4da2a
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-09-29 22:27:48 +0300

    Updated API key for embedly.

--- backend/allmywishes/apps/wish/utils.py
+++ backend/allmywishes/apps/wish/utils.py
@@ -1,14 +1,11 @@
 import urlparse
 import logging
 
-import requests
-#from BeautifulSoup import BeautifulSoup as bs
-#from readability.readability import Document
-
 from django.conf import settings
 from django.core.files.base import ContentFile
 from django.template.defaultfilters import truncatechars
 
+import requests
 from embedly import Embedly
 
 
@@ -31,59 +28,6 @@ def fetch_image(url):
         return None
     name = urlparse.urlparse(url).path.split('/')[-1]
     return name, ContentFile(fetch_data.content)
-
-
-#class ParseWishItem(object):
-#    """
-#        Own parser need to be improved!
-#    """
-#
-#    def __init__(self, url):
-#        self.url = url
-#        try:
-#            self.fetched_data = requests.get(self.url)
-#            self.content = self.fetched_data.content
-#        except requests.RequestException:
-#            logger.error("Incorrect url %s" % url)
-#            assert False, "Provided url - unreachable!"
-#
-#    def get_title(self):
-#        return Document(self.content).summary()
-#
-#    def get_description(self):
-#        return Document(self.content).short_title()
-#
-#    def get_biggest_image(self):
-#        """
-#            making head requests for all images on page to find biggest one by
-#            content length information in headers;
-#            We believe it will not be background image :)
-#        """
-#        images_with_size = []
-#        soup = bs(self.content)
-#        for image in soup.findAll("img"):
-#            name = image["src"].split("/")[-1]
-#            if image["src"].lower().startswith("http") and not name.endswith("gif"):
-#                src = image["src"].lower()
-#                try:
-#                    r = requests.head(src)
-#                    size = int(r.headers['content-length'])
-#                    print size
-#                except requests.RequestException:
-#                    pass
-#                else:
-#                    images_with_size.append(
-#                        (src, size)
-#                    )
-#        # We believe it will not be background image :)
-#        biggest_image = max(images_with_size, key=lambda x: x[1])
-#        return biggest_image
-#
-#    def get_og_image(self):
-#        pass
-#
-#    def get_price(self):
-#        pass
 
 
 class EmbedlyParser(object):
