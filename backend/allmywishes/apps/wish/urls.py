a3be166e51e474e006034fc8f5ed4aa74ec5718f
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-09-29 23:15:13 +0300

    Configured mine/public urls. Fixed delete functionality.

--- backend/allmywishes/apps/wish/urls.py
+++ backend/allmywishes/apps/wish/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, url
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from wish import views as wish_views
-
 
 urlpatterns = format_suffix_patterns(patterns(
     'wish.views',
 
     url(r'^parse$', 'wish_parse', name='wish-parse'),
-    url(r'^(?P<pk>\d+)$', wish_views.wish_retrieve_update_destroy, name='wish-retrieve-update-destroy'),
-    url(r'^discover$', wish_views.wish_discover, name='wish-discover'),
+    url(r'^mine$', 'wish_list_create', name='wish-list-create-mine'),
+    url(r'^mine/(?P<pk>\d+)$', 'wish_retrieve_update_destroy', name='wish-retrieve-update-destroy'),
+    url(r'^discover$', 'wish_discover', name='wish-discover'),
 ))
--- backend/allmywishes/apps/wish/views.py
+++ backend/allmywishes/apps/wish/views.py
@@ -18,12 +18,7 @@ class WishListCreate(generics.ListCreateAPIView):
     paginate_by = 10
 
     def get_queryset(self):
-        try:
-            account = Account.objects.get(slug=self.kwargs.get("slug"))
-        except Account.DoesNotExist:
-            return []
-        else:
-            return self.queryset.filter(account=account).all()
+        return self.queryset.filter(account=self.request.user).all()
 
     def pre_save(self, obj):
         if self.request.user.is_authenticated():
@@ -55,10 +50,8 @@ class WishRetrieve(generics.RetrieveAPIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
-        user = self.request.user
-        user_slug = self.kwargs.get("slug")
-        user_dst = get_object_or_404(Account, slug=user_slug)
-        return user_dst.wishes.all()
+        account = get_object_or_404(Account, slug=self.kwargs.get("slug"))
+        return self.queryset.filter(account=account).all()
 
 wish_retrieve = WishRetrieve.as_view()
 
