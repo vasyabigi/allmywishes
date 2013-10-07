c6d66896b0a39603b5c7c66e7499abdcfe805352
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2013-10-07 13:58:41 +0300

    Added facebook comments to details.

--- frontend/app/scripts/controllers/wishDetails.js
+++ frontend/app/scripts/controllers/wishDetails.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('WishDetailsCtrl', ['$scope', '$route', 'Restangular', function ($scope, $route, Restangular) {
+  .controller('WishDetailsCtrl', ['$scope', '$route', 'Restangular', '$window', function ($scope, $route, Restangular, $window) {
+    $scope.pageUrl = $window.location.href;
+
     var account = Restangular.one('accounts', $route.current.params.slug),
         wish = account.one('wishes', $route.current.params.wishSlug);
 
